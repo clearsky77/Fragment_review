3edb1c0e073a2777312e519dd34f7b38433c8180
clearsky77 <clearsky3264@gmail.com>
2021-09-29 21:06:10 +0900

    프래그먼트- 코틀린 파일- onActivityCreated 활용 (화면이 완성되면 실행되는 함수다. c.f.onCreat함수)

--- app/src/main/java/com/clearsky77/fragment_review/fragments/FirstFragment.kt
+++ app/src/main/java/com/clearsky77/fragment_review/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.clearsky77.fragment_review.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.clearsky77.fragment_review.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() { //상속을 받아야 편하다. androidx로 임포트
     override fun onCreateView(
@@ -16,4 +18,11 @@ class FirstFragment : Fragment() { //상속을 받아야 편하다. androidx로 
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) { // 화면이 완성되면 실행되는 함수. c.f.onCreat함수.)
+        super.onActivityCreated(savedInstanceState)
+
+        logBtn.setOnClickListener {
+            Log.d("첫 프래그먼트", "버튼 눌림")
+        }
+    }
 }
