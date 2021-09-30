969546845ce0d07d691637683110b8e555e0dd52
clearsky77 <clearsky3264@gmail.com>
2021-09-30 23:45:07 +0900

    두번째 프래그먼트 메인 화면에 배치 (토스트 띄우기 기능 <-에러남)

---
+++ app/src/main/java/com/clearsky77/fragment_review/fragments/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.clearsky77.fragment_review.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.clearsky77.fragment_review.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        toastBtn.setOnClickListener {
+            //Toast.makeText(this, "프래그먼트에서 토스트 띄우기", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
